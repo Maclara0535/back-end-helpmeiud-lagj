bea2f636caf3710970d77ccf1d83e6d428e88c7b
Maclara0535 <libardo.gomez@est.iudigital.edu.co>
2022-07-28 15:19:59 -0500

    :construction: entities, service - repository - and ctrl roles

--- src/main/java/co/edu/iudigital/model/Caso.java
+++ src/main/java/co/edu/iudigital/model/Caso.java
@@ -1,164 +1,171 @@
 package co.edu.iudigital.model;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Objects;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.PrePersist;
+import javax.persistence.Table;
 
-public class Caso implements Serializable{
+@Entity
+@Table(name = "casos")
+public class Caso implements Serializable {
 
 	/**
 	 * 
 	 */
-	private static final long serialVersionUID = -8169358650902209853L;
-	
-	    //id INT NOT NULL AUTO_INCREMENT,
-		@Id //PRIMARY KEY(id)
+	private static final long serialVersionUID = 7509375994430053778L;
+
+	//id INT NOT NULL AUTO_INCREMENT,
+		//PRIMARY KEY(id)
+		@Id
 		@GeneratedValue(strategy = GenerationType.IDENTITY)
 	    private Long id;
 	    
 		//fecha_hora DATETIME NULL DEFAULT now(),
-	    @Column(name = "fecha hora")
-	    private Double fechaHora;
+		@Column(name = "fecha_hora")
+		private LocalDateTime fechaHora;
 	    
 		//latitud FLOAT NULL,
-	    @Column(name = "latitud")
-	    private Double latitud;
+		private Float latitud;
 	    
 		//longitud FLOAT NULL,
-	    @Column(name = "longitud")
-	    private Double longitud;
+		private Float longitud;
 	    
 		//altitud FLOAT NULL,
-	    @Column(name = "altitud")
-	    private Double altitud;
-	    
+		private Float altitud;
 	    
 		//visible TINYINT NULL DEFAULT 1,
-	    @Column(columnDefinition = "NULL DEFAULT 1")
-		  private Boolean enabled;
+		private Boolean visible;
 	   
 		//descripcion VARCHAR(200) NULL,
-	    @Column(name = "descripcion", length = 200)
+		@Column(name = "descripcion", length = 200)
 		private String descripcion;
-
 		
 	    //url_map TEXT NULL,
-	    @Column(name = "url_map")
-		 private Boolean urlMap;
-	   
-	    // rmi_url TEXT NULL,
-	    @Column(name = "rmi_url")
-		 private Boolean rmiUrl;
-	   
-	    // usuarios_id INT NOT NULL,
-	    @Column(name = "usuarios", length = 120, nullable = false)
-		private String usuariosId;
-	    
+		@Column(name = "url_map")
+		private String urlMap;
+		
+	   // rmi_url TEXT NULL,
+		@Column(name = "rmi_url")
+		private String rmiUrl;
+		
+	   // usuarios_id INT NOT NULL,
+		//FOREIGN KEY (usuarios_id) REFERENCES usuarios(id),
+		@ManyToOne(cascade = CascadeType.ALL)
+		@JoinColumn(name = "usuarios_id")
+		private Usuario usuario;
+		
 	    //delitos_id INT NOT NULL,
-	    @Column(name = "delitos_id", length = 200, nullable = false)
-		private String delitosId;
+	    //FOREIGN KEY (delitos_id) REFERENCES delitos(id)
+		@ManyToOne(cascade = CascadeType.ALL)
+		@JoinColumn(name = "delitos_id")
+		private Delito delito;
+		
+		@PrePersist
+		public void init() {
+			if(Objects.isNull(fechaHora)) {//fechaHora==null
+				fechaHora = LocalDateTime.now();
+			}
+			if(Objects.isNull(visible)) {
+				visible = true;
+			}
+		}
 
 		public Long getId() {
 			return id;
 		}
 
-		public void setId(Long id) {
-			this.id = id;
-		}
-
-		public Double getFechaHora() {
+		public LocalDateTime getFechaHora() {
 			return fechaHora;
 		}
 
-		public void setFechaHora(Double fechaHora) {
-			this.fechaHora = fechaHora;
-		}
-
-		public Double getLatitud() {
+		public Float getLatitud() {
 			return latitud;
 		}
 
-		public void setLatitud(Double latitud) {
-			this.latitud = latitud;
-		}
-
-		public Double getLongitud() {
+		public Float getLongitud() {
 			return longitud;
 		}
 
-		public void setLongitud(Double longitud) {
-			this.longitud = longitud;
-		}
-
-		public Double getAltitud() {
+		public Float getAltitud() {
 			return altitud;
 		}
 
-		public void setAltitud(Double altitud) {
-			this.altitud = altitud;
-		}
-
-		public Boolean getEnabled() {
-			return enabled;
-		}
-
-		public void setEnabled(Boolean enabled) {
-			this.enabled = enabled;
+		public Boolean getVisible() {
+			return visible;
 		}
 
 		public String getDescripcion() {
 			return descripcion;
 		}
 
+		public String getUrlMap() {
+			return urlMap;
+		}
+
+		public String getRmiUrl() {
+			return rmiUrl;
+		}
+
+		public Usuario getUsuario() {
+			return usuario;
+		}
+
+		public Delito getDelito() {
+			return delito;
+		}
+
+		public void setId(Long id) {
+			this.id = id;
+		}
+
+		public void setFechaHora(LocalDateTime fechaHora) {
+			this.fechaHora = fechaHora;
+		}
+
+		public void setLatitud(Float latitud) {
+			this.latitud = latitud;
+		}
+
+		public void setLongitud(Float longitud) {
+			this.longitud = longitud;
+		}
+
+		public void setAltitud(Float altitud) {
+			this.altitud = altitud;
+		}
+
+		public void setVisible(Boolean visible) {
+			this.visible = visible;
+		}
+
 		public void setDescripcion(String descripcion) {
 			this.descripcion = descripcion;
 		}
 
-		public Boolean getUrlMap() {
-			return urlMap;
-		}
-
-		public void setUrlMap(Boolean urlMap) {
+		public void setUrlMap(String urlMap) {
 			this.urlMap = urlMap;
 		}
 
-		public Boolean getRmiUrl() {
-			return rmiUrl;
-		}
-
-		public void setRmiUrl(Boolean rmiUrl) {
+		public void setRmiUrl(String rmiUrl) {
 			this.rmiUrl = rmiUrl;
 		}
 
-		public String getUsuariosId() {
-			return usuariosId;
+		public void setUsuario(Usuario usuario) {
+			this.usuario = usuario;
 		}
 
-		public void setUsuariosId(String usuariosId) {
-			this.usuariosId = usuariosId;
+		public void setDelito(Delito delito) {
+			this.delito = delito;
 		}
-
-		public String getDelitosId() {
-			return delitosId;
-		}
-
-		public void setDelitosId(String delitosId) {
-			this.delitosId = delitosId;
-		}
-
-		public static long getSerialversionuid() {
-			return serialVersionUID;
-		}
-	    
-	    //FOREIGN KEY (usuarios_id) REFERENCES usuarios(id),
-	    //FOREIGN KEY (delitos_id) REFERENCES delitos(id)
-	
-
-}
-
-
-
-
+		
+	}
--- src/main/java/co/edu/iudigital/model/Delito.java
+++ src/main/java/co/edu/iudigital/model/Delito.java
@@ -2,25 +2,29 @@ package co.edu.iudigital.model;
 
 import java.io.Serializable;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "delitos")
-
 public class Delito implements Serializable{
-		
-		/**
+
+
+	/**
 	 * 
 	 */
-	private static final long serialVersionUID = 1221059064170099280L;
-	
-	    //id INT NOT NULL AUTO_INCREMENT
-		@Id //PRIMARY KEY (id)
+	private static final long serialVersionUID = 6225803848687364814L;
+
+	//id INT NOT NULL AUTO_INCREMENT
+		//PRIMARY KEY (id)
+		@Id
 		@GeneratedValue(strategy = GenerationType.IDENTITY)
 	    private Long id;
 		
@@ -30,39 +34,61 @@ public class Delito implements Serializable{
 		
 	    //descripcion TEXT NULL
 		private String descripcion;
+	    	
+		
+		//usuarios_id INT NOT NULL
+	    //FOREIGN KEY (usuarios_id) REFERENCES usuarios(id)
+		@ManyToOne(cascade = CascadeType.ALL) // por default: fetch = EAGER
+		@JoinColumn(name = "usuarios_id")
+		private Usuario usuario;
 
 		public Long getId() {
 			return id;
 		}
 
-		public void setId(Long id) {
-			this.id = id;
-		}
+
 
 		public String getNombre() {
 			return nombre;
 		}
 
-		public void setNombre(String nombre) {
-			this.nombre = nombre;
-		}
+
 
 		public String getDescripcion() {
 			return descripcion;
 		}
 
+
+
+		public Usuario getUsuario() {
+			return usuario;
+		}
+
+
+
+		public void setId(Long id) {
+			this.id = id;
+		}
+
+
+
+		public void setNombre(String nombre) {
+			this.nombre = nombre;
+		}
+
+
+
 		public void setDescripcion(String descripcion) {
 			this.descripcion = descripcion;
 		}
 
-		public static long getSerialversionuid() {
-			return serialVersionUID;
+
+
+		public void setUsuario(Usuario usuario) {
+			this.usuario = usuario;
 		}
 		
-	    
-		//usuarios_id INT NOT NULL
-	    //FOREIGN KEY (usuarios_id) REFERENCES usuarios(id)
-		// TODO: colocar la asociación & getteres y setteres
+		
+	}
 
 
-}
